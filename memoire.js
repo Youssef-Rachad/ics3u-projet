5f8fd2e9167cd93278166ff16a721eb6a512ef12
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2021-01-13 09:29:45 -0500

    meilleur interface, boutons, polices

--- memoire.js
+++ memoire.js
@@ -1,9 +1,16 @@
 const repete_mot = (mot, compte) => compte > 1? mot + repete_mot(mot, compte-1) : mot;
-const chiffre_dans_une_fourchette = (nombre) => nombre < 11 && nombre > 1? nombre : parseInt(Math.random() * 10);
+const chiffre_dans_une_fourchette = (nombre) => nombre < 6 && nombre > 2? nombre : parseInt(Math.random() * 4) + 2;
 var num = chiffre_dans_une_fourchette(parseInt(prompt("longueur du quadrillage")));
+
 for(let i=0; i<num; i++){
     document.getElementById("jeu").innerHTML += `<div class="rangee">`+repete_mot(`<div class="carreau"></div>`, num)+ `</div>`;
 }
-var carreaux = document.getElementsByClassName("carreau");
+var carreaux = Array.from(document.getElementsByClassName("carreau"));
+carreaux.forEach((carreau)=>{
+    carreau.addEventListener("click", (e)=>{
+        e.target.classList.add("active");
+        setTimeout(()=>{e.target.classList.remove("active")},1000);
+    })
+})
 
 
