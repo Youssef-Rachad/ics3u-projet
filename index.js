9d585c2f39425ed83a38a70164da12852eb38ce7
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2021-01-15 17:30:19 -0500

    ajout d'une fenetre au lieu d'un prompt()

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 intro_img = document.getElementById("intro_img"); 
-intro_img.src = "./cerveau.png";
+intro_img.src = "./cerveau-fleche.png";
 intro_img.addEventListener("click",(e)=>{
     let i = 150;
     for(let j = 0; j<8; j+=2){
         setTimeout(()=>{e.target.src = "./eclat.png"},j*i);
         setTimeout(()=>{e.target.src = "./cerveau.png"},(j+1)*i);
     }
-    setTimeout(()=>{window.location = "./memoire.html";},15*150);
-    
+    setTimeout(()=>{
+        intro_img.src = "./cerveau-fleche.png";
+        window.location = "./memoire.html";
+    },15*150);
+
 });
--- memoire.js
+++ memoire.js
@@ -1,20 +1,52 @@
+var entree = document.getElementById("entree");
+var btnPlus = document.getElementById("btn-plus");
+var btnMoins = document.getElementById("btn-moins");
+var carreaux = [];
 //-----AU DÉMARRAGE-----\\
 const repete_mot = (mot, compte) => compte > 1? mot + repete_mot(mot, compte-1) : mot;
-const chiffre_dans_une_fourchette = (nombre) => nombre < 6 && nombre > 2? nombre : parseInt(Math.random() * (6 - 2)) + 2;
-var num = chiffre_dans_une_fourchette(parseInt(prompt("longueur du quadrillage,\n un nombre entre 2 et 5 (inclu)")));
-
-for(let i=0; i<num; i++){
-    document.getElementById("jeu").innerHTML += `<div class="rangee">`+repete_mot(`<div class="carreau"></div>`, num)+ `</div>`;
+const chiffre_dans_une_fourchette = (nombre) => nombre <= 5 && nombre >= 2? nombre : parseInt(Math.random() * (6 - 2)) + 2;
+window.onload = ()=>{
+    var nombre = document.getElementById("nombre");
+    entree.style.display = "block";
+    btnPlus.addEventListener("click", () => nombre.innerText = (parseInt(nombre.innerText) + 1) <= 5? parseInt(nombre.innerText) + 1 : 5);
+    btnMoins.addEventListener("click", () => nombre.innerText = (parseInt(nombre.innerText) - 1) >= 2? parseInt(nombre.innerText) - 1 : 2); 
 }
+document.getElementById("btn-go").addEventListener("click", ()=>{
+    entree.style.display = "none";  
+    for(let i=0; i<nombre.innerText; i++){
+        document.getElementById("jeu").innerHTML += `<div class="rangee">`+repete_mot(`<div class="carreau"></div>`, nombre.innerText)+ `</div>`;
+    }
+    carreaux = Array.from(document.getElementsByClassName("carreau"));
+    carreaux.forEach((carreau)=>{
+        carreau.addEventListener("click", (e)=>{
+            if(jouer == false){
+                return false;
+            }
+            val = carreaux.indexOf(e.target);
+            clickBtn(e.target, 1000);
+            if(val != indexes.shift()){
+                setTimeout(()=>alert("BOOO"), 1100);
+                btnDepart.disabled = false;
+                jouer = false;
+            }else{
+                position++;
+                if(indexes.length == 0){
+                    setTimeout(()=>alert("GAGNÉ"), 1100);
+                    position = 0;
+                    btnDepart.disabled = false;
+                    jouer = false;
+                }
+            }
+        });
+    });
+});
 
 //-----MÉCANISME DES TUILES-----\\
-
+var btnDepart = document.getElementById("btn-depart");
 function clickBtn(boutton, delai){
     boutton.style.animation = 'activation 1s 1';
     timeoutID = setTimeout(()=>boutton.style.animation = '', delai);
 }
-var carreaux = Array.from(document.getElementsByClassName("carreau"));
-var btnDepart = document.getElementById("depart");
 //
 //-----ALGORITHMES DE HASARD-----\\
 //maximum exclusif & minimum inclusif
@@ -32,38 +64,20 @@ var val = -1;
 var position = 0;
 var indexes = [];
 //-----ÉCOUTEURS D'ÉVÉNEMENTS-----\\
-carreaux.forEach((carreau)=>{
-    carreau.addEventListener("click", (e)=>{
-        if(jouer == false){
-            return false;
-        }
-        val = carreaux.indexOf(e.target);
-        clickBtn(e.target, 1000);
-        if(val != indexes.shift()){
-            alert("BOOO");
-            btnDepart.disabled = false;
-            jouer = false;
-        }else{
-            position++;
-
-            if(indexes.length == 0){
-                alert("GAGNÉ");
-                position = 0;
-                btnDepart.disabled = false;
-                jouer = false;
-            }
-        }
-    });
-});
 btnDepart.addEventListener("click", ()=>{
     btnDepart.disabled = true;
     //for(let i = 0; i<carreaux.length; i++){
     //    setTimeout(() => clickBtn(carreaux[i],1000), i * 1000);
     //}
+    var num = parseInt(nombre.innerText);
+    console.log(num);
     indexes = tableauHasard(3, num);
     for(let i = 0; i < indexes.length; i++){
-        setTimeout(() => clickBtn(carreaux[indexes[i]], 1000), i * 1000);
+        setTimeout(() => clickBtn(carreaux[indexes[i]], 1000), i * 1100);
     }
-    setTimeout(() => jouer = true, indexes.length * 1000);
+    setTimeout(() =>{
+        jouer = true; 
+        document.querySelectorAll(".carreau").forEach(element => element.setAttribute("style", "cursor:pointer"));
+    }, indexes.length * 1000);
 
 });
