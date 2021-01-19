f7abc6a5a2638a5c96173daee79a159f69d99761
Youssef-Rachad <69124029+Youssef-Rachad@users.noreply.github.com>
2021-01-19 16:24:32 -0500

    images dans dossier séparé

--- aide.js
+++ aide.js
@@ -1,19 +1,19 @@
 var contenu = [
     {
         "pos":0, 
-        "h2":"Démarrer le jeu", "src":"./tutoriel-accueil.png", "alt": "L'accueil",  
+        "h2":"Démarrer le jeu", "src":"./image/tutoriel-accueil.png", "alt": "L'accueil",  
         "p":"Pour débuter la partie, appuyer sur le cerveau indiqué par des flèches jaunes.Le but du jeu est de se souvenir d'une séquence de tuiles qui clignoteront lorsqu'une partie débute. "
     },
     {
-        "h2":"Entraîner votre cerveau", "src":"tutoriel-jeu.png", "alt": "Le jeu", 
+        "h2":"Entraîner votre cerveau", "src":"./image/tutoriel-jeu.png", "alt": "Le jeu", 
         "p":"Cliquez les tuiles dans le bon ordre pour obtenir des points. Plusieurs rondes se jouent dans un même niveau et chaque niveau accorde des points supplémentaires!"
     },
     {
-        "h2":"Évaluez votre performance", "src":"tutoriel-stats.png", "alt": "Les statistiques", 
+        "h2":"Évaluez votre performance", "src":"./image/tutoriel-stats.png", "alt": "Les statistiques", 
         "p":"Vous êtes fort, même trop fort! Mais quelle est votre puissance quantitative? <br> Visitez la page des statistiques en cliquant s    ur le cerveau en haut à droite."
     },
     {
-        "h2":"La page à propos", "src":"tutoriel-apropos.png", "alt": "Les infos", 
+        "h2":"La page à propos", "src":"./image/tutoriel-apropos.png", "alt": "Les infos", 
         "p":"N'oubliez pas de visiter la page à-propos qui indique quelques infos sur le site. <br> Vous y trouverez aussi mes contacts incluant mon <a href='https://github.com/Youssef-Rachad/ics3u-projet'>répertoire GitHub</a>"
     }
 ];
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //-----PREMIERE IMAGE = CERVEAU AVEC FLECHE------\\
 intro_img = document.getElementById("intro_img"); 
-intro_img.src = "./cerveau-fleche.png";
+intro_img.src = "./image/cerveau-fleche.png";
 //-----AU CLIC POUR DÉCLENCHER LE JEU-----\\
 intro_img.addEventListener("click",(e)=>{
     //durée d'une image en millisecondes
@@ -17,14 +17,14 @@ intro_img.addEventListener("click",(e)=>{
          * (j+1)* permet de placer l'image du cerveau normal, sans interférer
          * avec l'image du cerveau avec éclairs
          **/
-        setTimeout(()=>{e.target.src = "./cerveau-eclair.png"},j*i);
-        setTimeout(()=>{e.target.src = "./cerveau.png"},(j+1)*i);
+        setTimeout(()=>{e.target.src = "./image/cerveau-eclair.png"},j*i);
+        setTimeout(()=>{e.target.src = "./image/cerveau.png"},(j+1)*i);
     }
     //Ici, remarquer le délai monstrueux
     //c'est ainsi pour donner le temps à l'animation de terminer
     //puis un petit délai pour un transition plus fluide vers le jeu
     setTimeout(()=>{
-        intro_img.src = "./cerveau-fleche.png";
+        intro_img.src = "./image/cerveau-fleche.png";
         window.location = "./memoire.html";
     },15*i);
 
